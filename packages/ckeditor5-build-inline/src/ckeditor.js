18b4a825f97a6be60b9b1bb15991a3e0c2a6fc80
Heychenko Victor <v.heychenko@gmail.com>
2020-11-14 17:48:20 +0200

    !

--- packages/ckeditor5-build-inline/src/ckeditor.js
+++ packages/ckeditor5-build-inline/src/ckeditor.js
@@ -4,99 +4,156 @@
  */
 
 // The editor creator to use.
-import InlineEditorBase from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor';
-
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent';
-import Link from '@ckeditor/ckeditor5-link/src/link';
-import List from '@ckeditor/ckeditor5-list/src/list';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-import Table from '@ckeditor/ckeditor5-table/src/table';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import InlineEditor from "@ckeditor/ckeditor5-editor-inline/src/inlineeditor.js";
+import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment.js";
+import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat.js";
+import Autolink from "@ckeditor/ckeditor5-link/src/autolink.js";
+import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote.js";
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold.js";
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials.js";
+import FontBackgroundColor from "@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js";
+import FontColor from "@ckeditor/ckeditor5-font/src/fontcolor.js";
+import FontSize from "@ckeditor/ckeditor5-font/src/fontsize.js";
+import Heading from "@ckeditor/ckeditor5-heading/src/heading.js";
+import HorizontalLine from "@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js";
+import HtmlEmbed from "@ckeditor/ckeditor5-html-embed/src/htmlembed.js";
+import Image from "@ckeditor/ckeditor5-image/src/image.js";
+import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption.js";
+import ImageInsert from "@ckeditor/ckeditor5-image/src/imageinsert.js";
+import ImageResize from "@ckeditor/ckeditor5-image/src/imageresize.js";
+import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle.js";
+import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar.js";
+import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload.js";
+import Indent from "@ckeditor/ckeditor5-indent/src/indent.js";
+import IndentBlock from "@ckeditor/ckeditor5-indent/src/indentblock.js";
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic.js";
+import Link from "@ckeditor/ckeditor5-link/src/link.js";
+import LinkImage from "@ckeditor/ckeditor5-link/src/linkimage.js";
+import List from "@ckeditor/ckeditor5-list/src/list.js";
+import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed.js";
+import MediaEmbedToolbar from "@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar.js";
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph.js";
+import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
+import RemoveFormat from "@ckeditor/ckeditor5-remove-format/src/removeformat.js";
+import SimpleUploadAdapter from "@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter.js";
+import Strikethrough from "@ckeditor/ckeditor5-basic-styles/src/strikethrough.js";
+import Subscript from "@ckeditor/ckeditor5-basic-styles/src/subscript.js";
+import Superscript from "@ckeditor/ckeditor5-basic-styles/src/superscript.js";
+import Table from "@ckeditor/ckeditor5-table/src/table.js";
+import TableCellProperties from "@ckeditor/ckeditor5-table/src/tablecellproperties";
+import TableProperties from "@ckeditor/ckeditor5-table/src/tableproperties";
+import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar.js";
+import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation.js";
+import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline.js";
 
 export default class InlineEditor extends InlineEditorBase {}
 
 // Plugins to include in the build.
 InlineEditor.builtinPlugins = [
-	Essentials,
-	UploadAdapter,
-	Autoformat,
-	Bold,
-	Italic,
-	BlockQuote,
-	CKFinder,
-	EasyImage,
-	Heading,
-	Image,
-	ImageCaption,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload,
-	Indent,
-	Link,
-	List,
-	MediaEmbed,
-	Paragraph,
-	PasteFromOffice,
-	Table,
-	TableToolbar,
-	TextTransformation
+    Alignment,
+    Autoformat,
+    Autolink,
+    BlockQuote,
+    Bold,
+    Essentials,
+    FontBackgroundColor,
+    FontColor,
+    FontSize,
+    Heading,
+    HorizontalLine,
+    HtmlEmbed,
+    Image,
+    ImageCaption,
+    ImageInsert,
+    ImageResize,
+    ImageStyle,
+    ImageToolbar,
+    ImageUpload,
+    Indent,
+    IndentBlock,
+    Italic,
+    Link,
+    LinkImage,
+    List,
+    MediaEmbed,
+    MediaEmbedToolbar,
+    Paragraph,
+    PasteFromOffice,
+    RemoveFormat,
+    SimpleUploadAdapter,
+    Strikethrough,
+    Subscript,
+    Superscript,
+    Table,
+    TableCellProperties,
+    TableProperties,
+    TableToolbar,
+    TextTransformation,
+    Underline,
 ];
 
 // Editor configuration.
 InlineEditor.defaultConfig = {
-	toolbar: {
-		items: [
-			'heading',
-			'|',
-			'bold',
-			'italic',
-			'link',
-			'bulletedList',
-			'numberedList',
-			'|',
-			'indent',
-			'outdent',
-			'|',
-			'imageUpload',
-			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
-			'undo',
-			'redo'
-		]
-	},
-	image: {
-		toolbar: [
-			'imageStyle:full',
-			'imageStyle:side',
-			'|',
-			'imageTextAlternative'
-		]
-	},
-	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
-		]
-	},
-	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
+    toolbar: {
+        items: [
+            "heading",
+            "|",
+            "bold",
+            "italic",
+            "underline",
+            "strikethrough",
+            "subscript",
+            "superscript",
+            "link",
+            "bulletedList",
+            "numberedList",
+            "|",
+            "alignment",
+            "indent",
+            "outdent",
+            "|",
+            "imageUpload",
+            "blockQuote",
+            "insertTable",
+            "mediaEmbed",
+            "undo",
+            "redo",
+        ],
+    },
+    image: {
+        styles: ["alignLeft", "alignCenter", "alignRight"],
+        resizeOptions: [
+            {
+                name: "imageResize:original",
+                label: "Original",
+                value: null,
+            },
+            {
+                name: "imageResize:50",
+                label: "50%",
+                value: "50",
+            },
+            {
+                name: "imageResize:75",
+                label: "75%",
+                value: "75",
+            },
+        ],
+        toolbar: [
+            "imageStyle:alignLeft",
+            "imageStyle:alignCenter",
+            "imageStyle:alignRight",
+            "|",
+            "imageResize",
+            "|",
+            "imageTextAlternative",
+            "|",
+            "linkImage",
+        ],
+    },
+    table: {
+        contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
+    },
+    // This value must be kept in sync with the language defined in webpack.config.js.
+    language: "ru",
 };
